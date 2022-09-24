9abed9e49bdc1c4ea735e1b2112b7822968b4b3c
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-09-24 16:10:33 -0400

    Modified settings and urls for media files

--- juegos_caribe_web/juegos_caribe_web/urls.py
+++ juegos_caribe_web/juegos_caribe_web/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('informaciones/', include('informaciones.urls')),
     path('medallero/', include('medallero.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
