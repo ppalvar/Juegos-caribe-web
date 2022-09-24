b31db2d0721ae393040cddd03db0f81c4dbc050c
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-09-24 16:08:51 -0400

    Created models and admins for "galeria" app

--- juegos_caribe_web/galeria/admin.py
+++ juegos_caribe_web/galeria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('description',)
+    list_filter  = ('description',)
--- juegos_caribe_web/galeria/models.py
+++ juegos_caribe_web/galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Image(models.Model):
+    description = models.CharField(max_length=100, null=False)
+    alt = models.CharField(max_length=20, null=False)
+    added = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=settings.MEDIA_ROOT)
+
+    class Meta:
+        ordering = ['-added']
+    
+    def __str__(self):
+        return self.description
